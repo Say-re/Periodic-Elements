171b188c626c9cfe266ae79cc90f8a053bee0f5c
Say-re <91860677+Say-re@users.noreply.github.com>
2022-09-14 21:10:46 -0700

    add boron

---
+++ empty-element-object.js
@@ -0,0 +1,33 @@
+{
+  "atomicMass": ,
+  "atomicNumber": "",
+  "boilingPoint": {
+    "unitOfMeasurement": "K",
+    "value":
+  },
+  "category": "",
+  "commonName": "",
+  "density": {
+    "unitOfMeasurement": "g/cm3",
+    "value":
+  },
+  "discovery": "",
+  "electronConfiguration": [
+    {
+      "shell": "1s",
+      "value": "2"
+    },
+    {
+      "shell": "2s",
+      "value": "2"
+    }
+  ],
+  "generalState": "solid",
+  "group": "",
+  "meltingPoint": {
+    "unitOfMeasurement": "K",
+    "value":
+  },
+  "period": "",
+  "symbol": ""
+}
